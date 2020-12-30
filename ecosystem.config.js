6a1404b178864e1584f7c817d6a24a3a6139a29d
chen yanfeng <992777621@qq.com>
2020-12-30 13:31:13 +0800

    ecosystem  commit

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps : [{
+    script: 'serve -l 8070 -c ../serve.json',
+    cwd:'/www/deewin-app-market/build',
+    watch: '.'
+  }],
+
+  deploy : {
+    production : {
+      user : 'SSH_USERNAME',
+      host : 'SSH_HOSTMACHINE',
+      ref  : 'origin/master',
+      repo : 'GIT_REPOSITORY',
+      path : 'DESTINATION_PATH',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
